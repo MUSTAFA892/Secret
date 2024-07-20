e18c98d647b5e533a537058e8b260356382410bb
MUSTAFA A <mustafatinwala6@gmail.com>
2024-07-20 15:37:24 +0530

    first commit

--- app.py
+++ app.py
@@ -1,76 +1,53 @@
-# Importing essential libraries
 from flask import Flask, render_template, request
 import pickle
-import numpy as np
-
-# Load the Random Forest CLassifier model
-filename = 'first-innings-score-lr-model.pkl'
-regressor = pickle.load(open(filename, 'rb'))
+import pandas as pd
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-	return render_template('index.html')
+# Load the model
+pipe = pickle.load(open('pipe.pkl', 'rb'))
 
-@app.route('/predict', methods=['POST'])
-def predict():
-    temp_array = list()
-    
+teams = [
+    'Australia', 'India', 'Bangladesh', 'New Zealand', 'South Africa', 
+    'England', 'West Indies', 'Afghanistan', 'Pakistan', 'Sri Lanka'
+]
+
+cities = [
+    'Colombo', 'Mirpur', 'Johannesburg', 'Dubai', 'Auckland', 'Cape Town', 
+    'London', 'Pallekele', 'Barbados', 'Sydney', 'Melbourne', 'Durban', 
+    'St Lucia', 'Wellington', 'Lauderhill', 'Hamilton', 'Centurion', 
+    'Manchester', 'Abu Dhabi', 'Mumbai', 'Nottingham', 'Southampton', 
+    'Mount Maunganui', 'Chittagong', 'Kolkata', 'Lahore', 'Delhi', 
+    'Nagpur', 'Chandigarh', 'Adelaide', 'Bangalore', 'St Kitts', 'Cardiff', 
+    'Christchurch', 'Trinidad'
+]
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    prediction = None
     if request.method == 'POST':
-        
-        batting_team = request.form['batting-team']
-        if batting_team == 'Chennai Super Kings':
-            temp_array = temp_array + [1,0,0,0,0,0,0,0]
-        elif batting_team == 'Delhi Daredevils':
-            temp_array = temp_array + [0,1,0,0,0,0,0,0]
-        elif batting_team == 'Kings XI Punjab':
-            temp_array = temp_array + [0,0,1,0,0,0,0,0]
-        elif batting_team == 'Kolkata Knight Riders':
-            temp_array = temp_array + [0,0,0,1,0,0,0,0]
-        elif batting_team == 'Mumbai Indians':
-            temp_array = temp_array + [0,0,0,0,1,0,0,0]
-        elif batting_team == 'Rajasthan Royals':
-            temp_array = temp_array + [0,0,0,0,0,1,0,0]
-        elif batting_team == 'Royal Challengers Bangalore':
-            temp_array = temp_array + [0,0,0,0,0,0,1,0]
-        elif batting_team == 'Sunrisers Hyderabad':
-            temp_array = temp_array + [0,0,0,0,0,0,0,1]
-            
-            
-        bowling_team = request.form['bowling-team']
-        if bowling_team == 'Chennai Super Kings':
-            temp_array = temp_array + [1,0,0,0,0,0,0,0]
-        elif bowling_team == 'Delhi Daredevils':
-            temp_array = temp_array + [0,1,0,0,0,0,0,0]
-        elif bowling_team == 'Kings XI Punjab':
-            temp_array = temp_array + [0,0,1,0,0,0,0,0]
-        elif bowling_team == 'Kolkata Knight Riders':
-            temp_array = temp_array + [0,0,0,1,0,0,0,0]
-        elif bowling_team == 'Mumbai Indians':
-            temp_array = temp_array + [0,0,0,0,1,0,0,0]
-        elif bowling_team == 'Rajasthan Royals':
-            temp_array = temp_array + [0,0,0,0,0,1,0,0]
-        elif bowling_team == 'Royal Challengers Bangalore':
-            temp_array = temp_array + [0,0,0,0,0,0,1,0]
-        elif bowling_team == 'Sunrisers Hyderabad':
-            temp_array = temp_array + [0,0,0,0,0,0,0,1]
-            
-            
+        batting_team = request.form['batting_team']
+        bowling_team = request.form['bowling_team']
+        city = request.form['city']
+        current_score = int(request.form['current_score'])
         overs = float(request.form['overs'])
-        runs = int(request.form['runs'])
         wickets = int(request.form['wickets'])
-        runs_in_prev_5 = int(request.form['runs_in_prev_5'])
-        wickets_in_prev_5 = int(request.form['wickets_in_prev_5'])
-        
-        temp_array = temp_array + [overs, runs, wickets, runs_in_prev_5, wickets_in_prev_5]
-        
-        data = np.array([temp_array])
-        my_prediction = int(regressor.predict(data)[0])
-              
-        return render_template('result.html', lower_limit = my_prediction-10, upper_limit = my_prediction+5)
+        last_five = int(request.form['last_five'])
 
+        balls_left = 120 - (overs * 6)
+        wickets_left = 10 - wickets
+        crr = current_score / overs
 
+        input_df = pd.DataFrame(
+            {'batting_team': [batting_team], 'bowling_team': [bowling_team], 'city': [city], 
+             'current_score': [current_score], 'balls_left': [balls_left], 
+             'wickets_left': [wickets], 'crr': [crr], 'last_five': [last_five]}
+        )
+
+        result = pipe.predict(input_df)
+        prediction = int(result[0])
+
+    return render_template('index.html', teams=sorted(teams), cities=sorted(cities), prediction=prediction)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
